99fe971590e06678de149ea10b87d5849665571a
alptugozates <omeralptugozates@gmail.com>
2023-11-21 17:27:52 +0300

    reducer ve add_movie case'i oluşturuldu

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+
 import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
 import movies from '../data.js'
 
@@ -7,17 +7,20 @@ const initialState = {
   appTitle: "IMDB Movie Database"
 }
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case DELETE_MOVIE:
       return {
         ...state,
         movies: state.movies.filter(item => (action.payload !== item.id))
       }
+    case ADD_MOVIE:
+      return {
+        ...state,
+        movies: [...state.movies, action.payload]
+      }
     default:
       return state;
   }
 }
-const store = createStore(reducer);
-
 export default reducer;
