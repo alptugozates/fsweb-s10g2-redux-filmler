ef84b8e3c4eb5aa644afb6210ac36325af5b7447
alptugozates <omeralptugozates@gmail.com>
2023-11-21 17:28:15 +0300

    favoriteMovie reducer'ı olusturuldu

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
-
+import reducer from './movieReducer';
 import movieReducer from './movieReducer';
+import favoritesReducer from './favoritesReducer';
+export default combineReducers({
+    movies: movieReducer,
+    favorites: favoritesReducer,
 
-export default movieReducer;
+})
