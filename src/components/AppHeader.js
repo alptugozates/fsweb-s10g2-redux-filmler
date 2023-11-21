931a9e3dc15769823f248059dd9842843964d569
alptugozates <omeralptugozates@gmail.com>
2023-11-21 19:00:39 +0300

    dispatch ile togleFavorites tetikler

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { toggleFavorites } from '../actions/favoritesActions';
 
 const AppHeader = (props) => {
+  const dispatch = useDispatch();
 
-  const displayFavorites = true;
   const appTitle = useSelector(state => state.movies.appTitle)
   console.log("appTitle:", appTitle)
+
+  const displayFavorites = useSelector(state => state.movies.displayFavorites);
+  console.log("displayFavorite:", displayFavorites)
+
+  const handleToggleFavorites = () => {
+    dispatch(toggleFavorites());
+  }
 
   return (
     <div className="flex justify-between items-center shadow rounded-md bg-white p-2 pl-3 my-3">
       <h2 className='text-zinc-600'>{appTitle}</h2>
       <div className="flex items-center gap-2">
-        <div className="myButton bg-blue-600 hover:bg-blue-500">
+        <div className="myButton bg-blue-600 hover:bg-blue-500" onClick={handleToggleFavorites}>
           <span>Favorileri {displayFavorites ? "gizle" : "göster"}</span>
         </div>
         <Link to="/movies" className="myButton bg-blue-600 hover:bg-blue-500">Tüm filmler</Link>
