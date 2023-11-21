ad787a55bcd0e8b498a1865976cfd498cc8542ae
alptugozates <omeralptugozates@gmail.com>
2023-11-21 18:59:25 +0300

    favoriteReducer'a case'ler eklendi

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,19 +1,28 @@
-
-
-
+import { ADD_FAVORITE, TOGGLE_FAVORITES, REMOVE_FAVORITE } from "../actions/favoritesActions";
 
 const initialStateFavorite = {
     favorites: [],
-    displayFavorites: false,
+    displayFavorites: true,
 };
+
 
 const favoritesReducer = (state = initialStateFavorite, action) => {
     switch (action.type) {
-        case 'ADD_FAVORITE':
+        case ADD_FAVORITE:
             return {
                 ...state,
                 favorites: [...state.favorites, action.payload],
             };
+        case REMOVE_FAVORITE:
+            return {
+                ...state,
+                favorites: state.favorites.filter(item => item.id !== action.payload),
+            }
+        case TOGGLE_FAVORITES:
+            return {
+                ...state,
+                displayFavorites: !state.displayFavorites,
+            }
         default:
             return state;
     }
